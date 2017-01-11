97010a5875dc8d8010674f6476ee5604f7eabc32
Levi Smith <levi@fynx.me>
2017-01-11 10:46:54 -0600

    renaming script to a single naming scheme, changing names in cfn template, adding additional setup to master node

--- scripts/groovy/masterConfig.groovy
+++ scripts/groovy/masterConfig.groovy
@@ -1,8 +1,11 @@
 import jenkins.model.*
+import hudson.model.*
 
 def instance = Jenkins.getInstance()
 
 //Master Executors Lock to 1
 instance.setNumExecutors(1)
+instance.setlabelString('master')
+instance.setMode(hudson.model.Node.Mode.EXCLUSIVE)
 
 instance.save()
